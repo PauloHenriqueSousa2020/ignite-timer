5d5c06abce9483c36f475d6513264db32a366875
PauloHenriqueSousa2020 <paulohenriquesousa1997@gmail.com>
2024-05-22 16:49:58 -0300

    feat: finish project

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,37 @@
+import * as S from './styles'
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext';
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <S.FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <S.TaskInput
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+
+      <label htmlFor="minutesAmount">durante</label>
+      <S.MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </S.FormContainer>
+  )
+}
